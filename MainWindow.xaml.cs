9af9a0bf02448d5a213b9edcb6bd88154688aa55
Noah <sovannarasay11@gmail.com>
2021-08-11 22:01:41 -0600

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Webscraper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Scraper scraper;
+        public MainWindow()
+        {
+            InitializeComponent();
+            scraper = new Scraper();
+            DataContext = scraper;
+        }
+
+        private void BtnScraper_Click(object sender, RoutedEventArgs e)
+        {
+            scraper.ScrapeData(TbPage.Text);
+        }
+
+        private void ItemExport_Click(object sender, RoutedEventArgs e)
+        {
+            scraper.Export();
+        }
+    }
+}
---
+++ Scraper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using CsvHelper;
+using HtmlAgilityPack;
+using System.Globalization;
+
+namespace Webscraper
+{
+    public class Scraper
+    {
+        private ObservableCollection<EntryModel> _entries = new ObservableCollection<EntryModel>();
+
+        public ObservableCollection<EntryModel> Entries
+        {
+            get { return _entries; }
+            set { _entries = value; }
+        }
+        public void ScrapeData(string page)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(page);
+
+            var Articles = doc.DocumentNode.SelectNodes("//*[@class = 'article-single']");
+
+            foreach (var article in Articles)
+            {
+                var header = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class = 'article-header']").InnerText);
+                var description = HttpUtility.HtmlDecode(article.SelectSingleNode(".//li[@class = 'article-copy']").InnerText);
+                Debug.Print($"Title: {header} \n Description: {description}");
+                _entries.Add(new EntryModel { Title = header, Description = description});
+            }
+        }
+
+        public void Export()
+        {
+            using (TextWriter tw = File.CreateText("SampleData.csv"))
+            {
+                using (CsvWriter cw = new CsvWriter(tw, new CultureInfo("en-US")))
+                {
+                    foreach (var entry in Entries)
+                    {
+                        cw.WriteRecord(entry);
+                        cw.NextRecord();
+                    }
+                }
+            }
+        }
+    }
+}
